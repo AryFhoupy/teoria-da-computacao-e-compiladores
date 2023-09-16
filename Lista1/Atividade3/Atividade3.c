b09d94912d055d5c195d915d7efef953d8ade31d
AryFhoupy <99850352+AryFhoupy@users.noreply.github.com>
2023-09-16 10:33:29 -0300

    Atividade3.c

--- Lista1/Atividade3/Atividade3.c
+++ Lista1/Atividade3/Atividade3.c
@@ -3,17 +3,17 @@
 
 int main() {
 
-float cotacao,dolar;
-
-printf("Informe a cotação do dolar em rais, por gentileza: \n");
-scanf("%f", &cotacao);
-printf("Quantos dólares você possui?\n");
-scanf("%f", &dolar);
-
-float reais = dolar * cotacao;
-
-printf("Você possui %.2f reais\n", reais);
-
-retun 0;
+  double cotacao,dolar;
+  
+  printf("Informe a cotação do dolar em rais, por gentileza: \n");
+  scanf("%lf", &cotacao);
+  printf("Quantos dólares você possui?\n");
+  scanf("%lf", &dolar);
+  
+  double reais = dolar * cotacao;
+  
+  printf("Você possui %.2lf reais\n", reais);
+  
+  return 0;
 
 }
