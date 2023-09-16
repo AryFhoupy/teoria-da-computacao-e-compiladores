46637b45e96d04e88ead632347e8ef1a89d85282
AryFhoupy <99850352+AryFhoupy@users.noreply.github.com>
2023-09-16 10:20:20 -0300

    Atividade10.c

--- Lista1/Atividade10/Atividade10.c
+++ Lista1/Atividade10/Atividade10.c
@@ -6,16 +6,16 @@ int main() {
     float comprimento, area, volume, raio;
     float pi = 3.141;
     
-    printf("Informe o raio do círculo\n");
+    printf("Informe o raio: \n");
     scanf("%f", &raio);
     
     comprimento = 2 * pi * raio;
     area = pi * (pow(raio,2));
     volume = (3.0 / 4.0) * pi * pow(raio,3);
     
-    printf("\nO raio da esfera é %.2f\n", comprimento);
-    printf("A area do esfera é %.2f\n", area);
-    printf("O volume do esfera é %.2f\n\n", volume);
+    printf("\nO raio da esfera é: \n%.2f  \n", comprimento);
+    printf("A area da esfera é: \n%.2f \n", area);
+    printf("O volume da esfera é: \n%.2f \n\n", volume);
     
     return 0;
     
