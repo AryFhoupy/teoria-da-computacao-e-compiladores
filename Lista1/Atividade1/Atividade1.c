03bd9945d8f5cb0cd6ae756b48b5ba3ac4b85200
AryFhoupy <99850352+AryFhoupy@users.noreply.github.com>
2023-09-16 10:22:26 -0300

    Atividade1.c

--- Lista1/Atividade1/Atividade1.c
+++ Lista1/Atividade1/Atividade1.c
@@ -3,20 +3,20 @@
 
 int main() {
 
-int a, b, c;
-
-
-printf ("\nInforme a primeira nota: ");
-scanf ("%d", &a);
-printf ("\nInforme a segunda nota: ");
-scanf ("%d", &b);
-printf ("\nInforme a terceira nota: ");
-scanf ("%d", &c);
-
-float x = (a+b+c)/3;
-
-printf("\nA média aritimética das três notas apresentadas é %.2f\n", x);
-
-return 0;
+  int a, b, c;
+  
+  
+  printf ("\nInforme a primeira nota: \n");
+  scanf ("%d", &a);
+  printf ("\nInforme a segunda nota: \n");
+  scanf ("%d", &b);
+  printf ("\nInforme a terceira nota: \n");
+  scanf ("%d", &c);
+  
+  float x = (a+b+c)/3;
+  
+  printf("\nA média aritimética das três notas apresentadas é: \n%.2f\n", x);
+  
+  return 0;
 
 }
