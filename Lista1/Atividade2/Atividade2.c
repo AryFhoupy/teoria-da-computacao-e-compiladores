14f0f9cb7e46f869d378f8c4ae3dec2d4a9be33d
AryFhoupy <99850352+AryFhoupy@users.noreply.github.com>
2023-09-16 10:26:45 -0300

    Atividade2.c

--- Lista1/Atividade2/Atividade2.c
+++ Lista1/Atividade2/Atividade2.c
@@ -2,22 +2,22 @@
 #include <math.h>//pow().
 
 int main() {
-
-int AnoNascimento, AnoAtual;
-
-
-printf ("\nEm que ano você nasceu?: ");
-scanf ("%d", &AnoNascimento);
-printf ("\nEm que ano você está? ");
-scanf ("%d", &AnoAtual);
-
-int IdadeAtual = AnoAtual - AnoNascimento;
-int Idade2050 = 2050 - AnoNascimento;
-
-
-printf("\nVocê tem %d anos\n", IdadeAtual);
-printf("\nEm 2050 você terá %d anos de idade\n", Idade2050);
-
-return 0;
+  
+  int AnoNascimento, AnoAtual;
+  
+  
+  printf ("Em que ano você nasceu? \n");
+  scanf ("%d", &AnoNascimento);
+  printf ("\nEm que ano você está? \n");
+  scanf ("%d", &AnoAtual);
+  
+  int IdadeAtual = AnoAtual - AnoNascimento;
+  int Idade2050 = 2050 - AnoNascimento;
+  
+  
+  printf("\nVocê tem %d anos\n", IdadeAtual);
+  printf("\nEm 2050 você terá %d anos de idade\n", Idade2050);
+  
+  return 0;
 
 }
