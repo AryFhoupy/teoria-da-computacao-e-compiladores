cc33e0a5f03b9d25b4b92c5a02d5733fbd7b6cbc
AryFhoupy <99850352+AryFhoupy@users.noreply.github.com>
2023-10-25 19:17:25 -0300

    Update Atividade5.c

--- Lista1/Atividade5/Atividade5.c
+++ Lista1/Atividade5/Atividade5.c
@@ -1,20 +1,26 @@
+/*
+Calcule e apresente a área de um losango. As diagonais maior e menor do losango
+devem ser informadas pelo usuário. OBS: AREA = (DIAGONAL MAIOR * DIAGONAL
+MENOR) / 2.
+*/
+
 #include <stdio.h>//printf (), scanf().
 #include <math.h>//pow().
 
 int main() {
 
-float DiagonalMenor, DiagonalMaior;
-
-printf("Informe qual o valor da diagonal maior do losango: \n");
-scanf("%f", &DiagonalMaior);
-
-printf("Informe qual o valor da diagonal menor do losango: \n");
-scanf("%f", &DiagonalMenor);
-
-float Area = (DiagonalMaior * DiagonalMenor)/2;
-
-printf("A área do losango é %.2f\n", Area);
-
-return 0;
+  float DiagonalMenor, DiagonalMaior;
+  
+  printf("Informe qual o valor da diagonal maior do losango: \n");
+  scanf("%f", &DiagonalMaior);
+  
+  printf("Informe qual o valor da diagonal menor do losango: \n");
+  scanf("%f", &DiagonalMenor);
+  
+  float Area = (DiagonalMaior * DiagonalMenor)/2;
+  
+  printf("A área do losango é %.2f\n", Area);
+  
+  return 0;
 
 }
