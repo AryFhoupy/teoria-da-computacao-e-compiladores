c28bccd77e939c4ca13f2ea51ed997330161d23f
AryFhoupy <99850352+AryFhoupy@users.noreply.github.com>
2023-10-25 19:12:06 -0300

    Update Atividade2.c

--- Lista1/Atividade2/Atividade2.c
+++ Lista1/Atividade2/Atividade2.c
@@ -1,3 +1,9 @@
+/*
+Receba o ano de nascimento de uma pessoa, o ano atual e imprima:
+a. A idade da pessoa no ano atual.
+b. A idade que a pessoa terá em 2050.
+*/
+
 #include <stdio.h>//printf (), scanf().
 #include <math.h>//pow().
 
