7002aaf7a51e762339ef9d7d4b13e42e164a1246
AryFhoupy <99850352+AryFhoupy@users.noreply.github.com>
2023-10-25 19:22:04 -0300

    Update Atividade10.c

--- Lista1/Atividade10/Atividade10.c
+++ Lista1/Atividade10/Atividade10.c
@@ -1,3 +1,11 @@
+/*
+Receba o raio, calcule e apresente:
+
+a. O comprimento de uma esfera: C = 2 * π * R.
+b. A área de uma esfera: A = π * R2
+c. O volume de uma esfera: V = 3⁄4 * π * R3
+*/
+
 #include <stdio.h>//printf (), scanf().
 #include <math.h>//pow().
 
